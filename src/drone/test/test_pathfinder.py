6a4057941bcc2558b9c6bfb0e29ffb1eb65f778e
Ryan <ryantoh99@gmail.com>
2024-01-07 18:37:18 +0800

    drone: remove tests for hardcoded pathfinder

--- src/drone/test/test_pathfinder.py
+++ src/drone/test/test_pathfinder.py
@@ -1,11 +1,13 @@
 from drone.pathfinder import *
 import pytest
 
-def test_find_next_step():
-        pf = DummyPathfinder(0, (0,0))
-        for i in range(len(HARDCODED_WAYPOINT_TUPLES)-1):
-            p1 = HARDCODED_WAYPOINT_TUPLES[i]
-            p2 = HARDCODED_WAYPOINT_TUPLES[i+1]
-            p2_test = pf.find_next_step(p1, None)
-            assert pytest.approx(p2_test[0]) == p2[0]
-            assert pytest.approx(p2_test[1]) == p2[1]
+#TODO
+
+# def test_find_next_step():
+#         pf = DummyPathfinder(0, (0,0))
+#         for i in range(len(HARDCODED_WAYPOINT_TUPLES)-1):
+#             p1 = HARDCODED_WAYPOINT_TUPLES[i]
+#             p2 = HARDCODED_WAYPOINT_TUPLES[i+1]
+#             p2_test = pf.find_next_step(p1, None)
+#             assert pytest.approx(p2_test[0]) == p2[0]
+#             assert pytest.approx(p2_test[1]) == p2[1]
