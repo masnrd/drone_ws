a2046e572690629e430c1ddcdfb0ba6e1d7172db
Ryan <ryantoh99@gmail.com>
2024-01-07 20:47:58 +0800

    mc: feat: handle status and ready messages

---
+++ src/mission_control/mission_control/maplib.py
@@ -0,0 +1,53 @@
+"""
+maplib:
+Contains functions for converting between LatLon values and 
+(x,y) coordinates (based on metres from a reference point)
+"""
+
+from dataclasses import dataclass
+from math import sqrt, atan2, asin, cos, sin, radians
+
+EARTH_RADIUS = 6378.137 * 1000 # Earth's radius in metres
+
+@dataclass
+class LatLon:
+    """ Point, defined by WGS84 latitude and longitude. Code adapted from http://www.movable-type.co.uk/scripts/latlong.html. """
+    lat: float
+    lon: float
+
+    def distFromPoint(self, refPt: 'LatLon') -> float:
+        """ Returns the Haversine distance from another point. """
+        lat1 = radians(refPt.lat)
+        lat2 = radians(self.lat)
+        dLat = radians(self.lat - refPt.lat)
+        dLon = radians(self.lon - refPt.lon)
+        h_lat = (1 - cos(dLat))/2
+        h_lon = (1 - cos(dLon))/2
+        return asin(sqrt(h_lat + (cos(lat1)*cos(lat2)*h_lon))) * (2 * EARTH_RADIUS)
+
+    def toXY(self, refPt: 'LatLon') -> 'PositionXY':
+        """ Converts from WGS84 coordinates (lat, lon) to a position vector relative to a reference point (x, y, refPt). """
+        # 1. find the bearing: http://www.movable-type.co.uk/scripts/latlong.html
+        lat1 = radians(refPt.lat)
+        lat2 = radians(self.lat)
+        dLon = radians(self.lon - refPt.lon)
+        y_temp = sin(dLon) * cos(lat2)
+        x_temp = (cos(lat1) * sin(lat2)) - (sin(lat1)*cos(lat2)*cos(dLon))
+        bearing = atan2(y_temp, x_temp) # in radians
+
+        # 2. find the distance (haversine)
+        d = self.distFromPoint(refPt)
+
+        # 3. calc vector
+        new_x = d*cos(bearing)
+        new_y = d*sin(bearing)
+        return PositionXY(new_x, new_y, refPt)
+    
+    def __repr__(self) -> str:
+        return f"({self.lat}, {self.lon})"
+
+@dataclass
+class PositionXY:
+    x: float
+    y: float
+    refPt: LatLon
--- src/mission_control/mission_control/mission_control_node.py
+++ src/mission_control/mission_control/mission_control_node.py
@@ -1,6 +1,106 @@
-def main():
-    print('Hi from mission_control.')
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+from enum import IntEnum
+from typing import Dict
+from .maplib import LatLon
+from mc_interface_msgs.msg import Ready, Status
+from mc_interface_msgs.srv import Command
 
+class DroneMode(IntEnum):
+    """ Drone mode (from perspective of Mission Control) """
+    DISCONNECTED = 0
+    READY        = 1  # Received READY from drone
+    IN_OPERATION = 2  # Received CMD_ACK from drone after sending CMD
+
+class ReportedDroneMode(IntEnum):
+    """ Drone mode reported by the drone itself """
+    UNKNOWN    = -3  # Drone has not reported yet
+    RTB        = -2  # Drone is returning to MC
+    IDLE       = -1  # Drone is idle
+    INIT       = 0   # Complete initialisation
+    CONNECT_FC = 1   # Connecting to Flight Controller (FC)
+    INIT_FC    = 2   # Initialisation of subscriptions and publishers
+    CONNECT_MC = 3   # Connecting to Mission Control (MC)
+    TRAVEL     = 4   # Moving to Sector
+    SEARCH     = 5   # Searching Sector
+
+class DroneState:
+    def __init__(self, drone_id: int, node: 'MCNode'):
+        self.drone_id = drone_id
+        self.mode = DroneMode.DISCONNECTED
+        self.reported_mode = ReportedDroneMode.UNKNOWN
+        self.reported_battery_percentage = 0.0
+        self.position = None
+        self.last_command = None
+
+        self.sub_ready = node.create_subscription(
+            Ready,
+            f"/drone_{drone_id}/out/ready",
+            node.mc_recv_ready,
+            node.qos_profile,
+        )
+        self.sub_status = node.create_subscription(
+            Status,
+            f"/drone_{drone_id}/out/status",
+            node.mc_recv_status,
+            node.qos_profile,
+        )
+        self.cmd_cli = node.create_client(
+            Command,
+            f"/drone_{drone_id}/srv/cmd",
+        )
+
+class MCNode(Node):
+    def __init__(self):
+        super().__init__("mission_control")
+        self.drones:Dict[int, DroneState] = {}
+        self.qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1
+        )
+
+        self.add_drone(69)  #TODO: Handle multiple drones
+        print("MISSION CONTROL: Initialised.")
+
+    def add_drone(self, drone_id: int):
+        self.drones[drone_id] = DroneState(drone_id, self)
+
+    def mc_send_command(self, command_id: int, drone_id: int):
+        command = Command.Request()
+        drone = self.drones[drone_id]
+        #TODO: Send request and handle response
+
+    def mc_recv_ready(self, msg: Ready):
+        """ MC receives a READY message """
+        # 1. Identify drone by ID
+        drone_id = msg.drone_id
+
+        # 2. Update drone status
+        self.drones[drone_id].mode = DroneMode.READY
+        print(f"MISSION CONTROL: Drone {drone_id} reports READY")
+
+    def mc_recv_status(self, msg: Status):
+        """ MC receives a STATUS update from a drone """
+        # 1. Identify drone by ID
+        drone_id = msg.drone_id
+
+        # 2. Update local record of drone position and drone state
+        self.drones[drone_id].position = LatLon(msg.lat, msg.lon)
+        self.drones[drone_id].reported_mode = ReportedDroneMode(msg.drone_mode)
+        self.drones[drone_id].reported_battery_percentage = msg.battery_percentage
+        print(f"MISSION CONTROL: Drone {drone_id}:\n\tPosition: {self.drones[drone_id].position}\n\tMode: {self.drones[drone_id].reported_mode}\n\tBattery: {self.drones[drone_id].reported_battery_percentage}%.")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    mc_node = MCNode()
+    
+    rclpy.spin(mc_node)
+
+    mc_node.destroy_node()
 
 if __name__ == '__main__':
     main()
