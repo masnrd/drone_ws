71538ccd08aa5d05c3366c3a606236ebad29c794
Ryan <ryantoh99@gmail.com>
2024-01-13 20:54:33 +0800

    fix: more accurate computation of NED points

--- src/mission_control/mission_control/maplib.py
+++ src/mission_control/mission_control/maplib.py
@@ -5,43 +5,28 @@ Contains functions for converting between LatLon values and
 """
 
 from dataclasses import dataclass
-from math import sqrt, atan2, asin, cos, sin, radians
+from pymap3d import ned2geodetic, geodetic2ned
+from pymap3d.ellipsoid import Ellipsoid
+from pymap3d.vincenty import vdist
 
 EARTH_RADIUS = 6378.137 * 1000 # Earth's radius in metres
+DEFAULT_ALTITUDE = 5.0 #TODO: have the px4 report the current altitude for more accurate computation
+ELLIPSOID = Ellipsoid.from_name("wgs84")
 
 @dataclass
 class LatLon:
-    """ Point, defined by WGS84 latitude and longitude. Code adapted from http://www.movable-type.co.uk/scripts/latlong.html. """
+    """ A geodesic point """
     lat: float
     lon: float
 
     def distFromPoint(self, refPt: 'LatLon') -> float:
-        """ Returns the Haversine distance from another point. """
-        lat1 = radians(refPt.lat)
-        lat2 = radians(self.lat)
-        dLat = radians(self.lat - refPt.lat)
-        dLon = radians(self.lon - refPt.lon)
-        h_lat = (1 - cos(dLat))/2
-        h_lon = (1 - cos(dLon))/2
-        return asin(sqrt(h_lat + (cos(lat1)*cos(lat2)*h_lon))) * (2 * EARTH_RADIUS)
+        """ Returns the Vincenty distance from a reference point. """
+        return vdist(self.lat, self.lon, refPt.lat, refPt.lon)[0]
 
     def toXY(self, refPt: 'LatLon') -> 'PositionXY':
         """ Converts from WGS84 coordinates (lat, lon) to a position vector relative to a reference point (x, y, refPt). """
-        # 1. find the bearing: http://www.movable-type.co.uk/scripts/latlong.html
-        lat1 = radians(refPt.lat)
-        lat2 = radians(self.lat)
-        dLon = radians(self.lon - refPt.lon)
-        y_temp = sin(dLon) * cos(lat2)
-        x_temp = (cos(lat1) * sin(lat2)) - (sin(lat1)*cos(lat2)*cos(dLon))
-        bearing = atan2(y_temp, x_temp) # in radians
-
-        # 2. find the distance (haversine)
-        d = self.distFromPoint(refPt)
-
-        # 3. calc vector
-        new_x = d*cos(bearing)
-        new_y = d*sin(bearing)
-        return PositionXY(new_x, new_y, refPt)
+        ned = geodetic2ned(self.lat, self.lon, DEFAULT_ALTITUDE, refPt.lat, refPt.lon, DEFAULT_ALTITUDE, ell=ELLIPSOID)
+        return PositionXY(ned[0], ned[1], refPt)
     
     def __repr__(self) -> str:
         return f"({self.lat}, {self.lon})"
@@ -51,3 +36,8 @@ class PositionXY:
     x: float
     y: float
     refPt: LatLon
+    
+    def toLatLon(self) -> LatLon:
+        """ Converts from a position vector relative to a reference point (x, y, refPt) to WGS84 coordinates (lat, lon) """
+        geodetic = ned2geodetic(self.x, self.y, -DEFAULT_ALTITUDE, self.refPt.lat, self.refPt.lon, DEFAULT_ALTITUDE, ell=ELLIPSOID)
+        return LatLon(geodetic[0], geodetic[1])
