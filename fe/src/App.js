a2f49e34dd09df5ac7d3e89259a8771136a895ba
TNBL265 <long_tran@mymail.sutd.edu.sg>
2024-01-24 15:34:13 +0800

    :sparkles: customize routes

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Map from "./components/Map.js";
+import Config from "./components/Config.js";
 
-function App() {
-  return <Map />;
-}
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Map />} />
+        <Route path="/config" element={<Config />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ fe/src/components/Config.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Config = () => {
+  return <div>Config Page</div>;
+};
+
+export default Config;
